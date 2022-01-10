fb41493bba14c75fe623e30c22a494b22a00e8f1
EdneiFNeto <edneifneto@gmail.com>
2022-01-10 16:55:35 -0300

    Update index

--- src/service/api.js
+++ src/service/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://192.168.1.108:3001",
+  headers: {'Authorization': 'Bearer '+localStorage.getItem('token')}
 });
 
 export { api };
